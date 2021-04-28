bfe21df7ef27bcd32f066c6786b09c7335bdc231
GracielaJazmin <gracielajazmin_chavez@ucol.mx>
2021-04-28 13:01:03 -0500

    design fixes, remove the keyboard in the log in input and add an alert after submitting the test results.
    Co-authored-by: Genesis Zarate <genesis.zarate94@gmail.com>

--- AlertaCovid/src/components/atoms/ResultAtoms/BtnResult.js
+++ AlertaCovid/src/components/atoms/ResultAtoms/BtnResult.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, Alert} from 'react-native';
 import {ResultStyles} from '../../../styles';
 import {constants} from '../../../utils/constants';
 import {test_positive} from '../../../redux/actions/HealthActions';
 import {connect} from 'react-redux';
 import {checkTest} from '../../../utils/functions';
-const Submit = ({testResult, test_positive}) => {
+const Submit = ({testResult, test_positive, navigation}) => {
+
+  const Alerta = () => {
+    Alert.alert(
+      'Submited successfully',
+      'Your data has been submited successfully',
+      [
+        {
+          text: 'ok'
+        },
+      ],
+      {cancelable: true},
+    );
+  };
+
+
+
   return (
     <TouchableOpacity
       style={ResultStyles.btnContainerSubmit}
-      onPress={() => checkTest(testResult, test_positive)}>
+      onPress={()=> Alerta(checkTest(testResult, test_positive))}>
       <Text style={ResultStyles.TextSubmit}>{constants.submit}</Text>
     </TouchableOpacity>
   );
